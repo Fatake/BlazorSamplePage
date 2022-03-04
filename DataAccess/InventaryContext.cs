5d73e2df7daa869619efff2fe573c42444e565a5
Fatake <pace_lozano@hotmail.com>
2022-03-04 12:15:08 -0600

    EntityFramework maping to Data Acces layer

---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess {
+    public class InventaryContext:DbContext {
+        // Capa de acceso a datos
+
+        // Referencia de las tablas y Entidades
+        public DbSet<ProductEntity> T_Products { get; set; }
+        public DbSet<CategoryEntity> T_Categories { get; set; }
+        public DbSet<InOutsEntity> T_InOuts { get; set; }
+        public DbSet<StorageEntity> T_Storages { get; set; }
+        public DbSet<WarehouseEntity> T_Warehouses { get; set; }
+
+    }
+}
